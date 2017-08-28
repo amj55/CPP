8464480a46a5a001ca105015c593a49aaf4c8762
Goofables <goofables@gmail.com>
2017-08-28 13:50:56 -0600

    Initial

---
+++ Basic-Test/Basic-Test/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Basic-Test
+//
+//  Created by Gaelin Shupe on 8/21/17.
+//  Copyright © 2017 Gaelin Shupe. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int SECRET = 11;
+const double RATE = 12.5;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    //cin >> var1;
+    //cout << "Var1:" << var1
+    return 0;
+}
---
+++ Lab1 Basics/Lab1 Basics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab1 Basics
+//
+//  Created by Gaelin Shupe on 8/28/17.
+//  Copyright © 2017 Gaelin Shupe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
