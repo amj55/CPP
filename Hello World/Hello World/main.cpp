08b94e4965d97f37b9010a223f97786a87099122
Goofables <goofables@gmail.com>
2017-08-28 14:17:33 -0600

    Readd Hello World

---
+++ Hello World/Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Gaelin Shupe on 8/18/17.
+//  Copyright © 2017 Gaelin Shupe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
