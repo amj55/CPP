0a3c5d1764a2431626ef99f03a98279cdfb35fb8
Goofables <goofables@gmail.com>
2017-08-29 14:05:23 -0600

    Finished Basics

--- Lab1 Basics/Lab1 Basics/main.cpp
+++ Lab1 Basics/Lab1 Basics/main.cpp
@@ -12,19 +12,27 @@ using namespace std;
 
 int main() {
     int SECRET = 11;
-    //double RATE = 12.5;
+    double RATE = 12.5;
     double num1, num2, newNum;
     string name;
-    //double hoursWorked, wages;
+    double hoursWorked, wages;
     cout << "Enter two numbers seperated by a space." << endl;
     cin >> num1 >> num2;
     cout << "First number = " << num1 << "\nSecond number = " << num2 <<endl;
-    newNum = num1 * 2.0 + num2 + SECRET;
+    newNum = num1 * 2.0 + num2;
     cout << "New number = " << newNum << endl;
-    
-    
-    
-    
-    
+    newNum += SECRET;
+    cout << "New number = " << newNum << endl;
+    cout << "Last name: " << endl;
+    cin >> name;
+    do {
+        cout << "Hours worked: " << endl;
+        cin >> hoursWorked;
+    } while (hoursWorked > 70 | hoursWorked < 0);
+    wages = hoursWorked * RATE;
+    cout << "Name: " << name <<
+        "\nPay Rate: $" << RATE <<
+        "\nHours Worked: " << hoursWorked <<
+        "\n\nSalary: $" << wages << endl;
     return 0;
 }
