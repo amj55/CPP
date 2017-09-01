30dbdc1b5cd21c6211fce8cb480f61beb7cebffe
Goofables <Goofables@gmail.com>
2017-09-01 14:12:55 -0600

    Added Lab2[D[D[D[/Fixed lab2[D2[Haa

---
+++ Lab2 Beetle Mania/main.cpp
@@ -0,0 +1,54 @@
+
+/**
+* Beetle Mania
+*
+* This is a small windows console text animation
+* It shows how to wright colored text and how to
+* put text at a fixed position in a console.
+**/
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const char *const ben[] = {
+    "  )))",
+    "  |||",
+    "  (((",
+    "  |||"
+};
+
+int main() {
+    HANDLE con = GetStdHandle(STD_OUTPUT_HANDLE);
+    int i, j;
+    COORD coord;
+    coord.X=0;
+    for (i=0, j=80; i<40; i++, j--) {
+        SetConsoleTextAttribute(con, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+        coord.X = i;
+        coord.Y = 10;
+        SetConsoleCursorPosition(con, coord);
+        printf(ben[i%4]);
+        coord.Y = 11;
+        SetConsoleCursorPosition(con, coord);
+        printf(" -0000:");
+        coord.Y = 12;
+        SetConsoleCursorPosition(con, coord);
+        printf(ben[i%4]);
+
+        Sleep(75);
+        if (i == 39) {
+            SetConsoleTextAttribute(con, FOREGROUND_RED | FOREGROUND_INTENSITY);
+            coord.Y = 11;
+            coord.X = j+2;
+            SetConsoleCursorPosition(con, coord);
+            printf("***");
+            Sleep(200);
+        }
+
+        system("cls");
+    }
+
+    SetConsoleTextAttribute(con, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    return 0;
+}
