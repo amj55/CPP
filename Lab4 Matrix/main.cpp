44f6c37b4fb8dc7e0583c107b35e904180619b6c
goofables <goofables@gmail.com>
2017-09-20 13:36:39 -0600

    Matrix starter code

--- Lab4 Matrix/main.cpp
+++ Lab4 Matrix/main.cpp
@@ -1,27 +1,60 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define SCREEN_WIDTH 80
-#define SCREEN_HEIGHT 25
+#define SCREEN_WIDTH 125
+#define SCREEN_HEIGHT 30
 
-int main( void ) {
-  HANDLE hOutput = (HANDLE)GetStdHandle( STD_OUTPUT_HANDLE );
 
-  COORD dwBufferSize = { SCREEN_WIDTH,SCREEN_HEIGHT };
-  COORD dwBufferCoord = { 0, 0 };
-  SMALL_RECT rcRegion = { 0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1 };
+int main(void) {
+    int c = 0;
+    int r = 0;
+    HANDLE hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
 
-  CHAR_INFO buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
+    COORD dwBufferSize = { SCREEN_WIDTH, SCREEN_HEIGHT };
+    COORD dwBufferCoord = { 0, 0 };
+    SMALL_RECT rcRegion = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
 
-  ReadConsoleOutput( hOutput, (CHAR_INFO *)buffer, dwBufferSize,
-  dwBufferCoord, &rcRegion );
+    CHAR_INFO buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
 
-  /*for (int i=0; i<16; i++) {
-        buffer[i+5][2].Char.AsciiChar = 'A';
-        buffer[i+5][4].Char.AsciiChar = i/10 +48;
-        buffer[i+5][5].Char.AsciiChar = i%10 +48;
-        buffer[i+5][2].Attributes = i;
-  }*/ // 2, 3, 10, 11
-  WriteConsoleOutput( hOutput, (CHAR_INFO *)buffer, dwBufferSize, dwBufferCoord, &rcRegion );
-  return 0;
+    ReadConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize, dwBufferCoord, &rcRegion);
+    /*for (int i=0; i<16; i++) {
+      buffer[i+5][2].Char.AsciiChar = 'A';
+      buffer[i+5][4].Char.AsciiChar = i/10 +48;
+      buffer[i+5][5].Char.AsciiChar = i%10 +48;
+      buffer[i+5][2].Attributes = i;
+    }*/ // 2, 3, 10, 11
+    while(true) {
+        SetConsoleTextAttribute(hOutput, 2);
+        for(r = 0; r < SCREEN_HEIGHT; r++) {
+            for(c = 0; c < SCREEN_WIDTH; c++) {
+                if(r == 0) {
+                    if(true/*(rand() % 3 + 1) % 2 == 0*/) {
+                        buffer[r][c].Char.AsciiChar = rand() % 74 + 48;
+                        buffer[r][c].Attributes = rand() % 2 + 10;
+                    }
+                } else
+                    buffer[r][c] = buffer [r - 1][c];
+                }
+            WriteConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize, dwBufferCoord, &rcRegion);
+            Sleep(100);
+        }
+
+
+        for(r = 0; r < SCREEN_HEIGHT; r++) {
+            for(c = 0; c < SCREEN_WIDTH; c++) {
+                buffer[r][c].Char.AsciiChar = ' ';
+            }
+        }
+    }
+    return 0;
 }
+
+
+
+
+
+
+
+
