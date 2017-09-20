3c9da822a3eb2340a22c62ca336cc46a45528015
goofables <goofables@gmail.com>
2017-09-20 08:15:00 -0600

    Worked on matrix. Picked colors

--- Lab4 Matrix/main.cpp
+++ Lab4 Matrix/main.cpp
@@ -1,9 +1,27 @@
-#include <iostream>
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 
-using namespace std;
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+int main( void ) {
+  HANDLE hOutput = (HANDLE)GetStdHandle( STD_OUTPUT_HANDLE );
+
+  COORD dwBufferSize = { SCREEN_WIDTH,SCREEN_HEIGHT };
+  COORD dwBufferCoord = { 0, 0 };
+  SMALL_RECT rcRegion = { 0, 0, SCREEN_WIDTH-1, SCREEN_HEIGHT-1 };
+
+  CHAR_INFO buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
+
+  ReadConsoleOutput( hOutput, (CHAR_INFO *)buffer, dwBufferSize,
+  dwBufferCoord, &rcRegion );
+
+  /*for (int i=0; i<16; i++) {
+        buffer[i+5][2].Char.AsciiChar = 'A';
+        buffer[i+5][4].Char.AsciiChar = i/10 +48;
+        buffer[i+5][5].Char.AsciiChar = i%10 +48;
+        buffer[i+5][2].Attributes = i;
+  }*/ // 2, 3, 10, 11
+  WriteConsoleOutput( hOutput, (CHAR_INFO *)buffer, dwBufferSize, dwBufferCoord, &rcRegion );
+  return 0;
 }
