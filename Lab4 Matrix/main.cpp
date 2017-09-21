f3321d8d7b523444af5edfef92d91351287e6b85
goofables <goofables@gmail.com>
2017-09-20 23:33:25 -0600

    Matrix fully working

--- Lab4 Matrix/main.cpp
+++ Lab4 Matrix/main.cpp
@@ -6,18 +6,21 @@
 *
 */
 
-
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 
-#define SCREEN_WIDTH 125
-#define SCREEN_HEIGHT 30
+//#define SCREEN_WIDTH 125
+//#define SCREEN_HEIGHT 30
+#define SCREEN_WIDTH 197
+#define SCREEN_HEIGHT 51
 
 
 int main(void) {
+    system("mode 650");
+    Sleep(1000);
     int c = 0;
     int r = 0;
     HANDLE hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
@@ -34,26 +37,50 @@ int main(void) {
       buffer[i+5][4].Char.AsciiChar = i/10 +48;
       buffer[i+5][5].Char.AsciiChar = i%10 +48;
       buffer[i+5][2].Attributes = i;
-    }*/ // 2, 3, 10, 11
+    }*/ // 2, 3, 10, 11   33-126   -128 -1
+    /*for(char c = 1; c != 0; c++) {
+            std::cout << (int)c << "  " << c << std::endl;
+    }
+    std::cout << "Done" << std::endl;
+    Sleep(1000000);*/
+    int rate = 0;
     while(true) {
         for(r = SCREEN_HEIGHT - 1; r >= 0; r--) {
             for(c = 0; c < SCREEN_WIDTH; c++) {
                 if(r == 0) {
                     if(buffer[r][c].Char.AsciiChar == 32) {
-                        if(rand() % 35 == 0) {
-                            buffer[r][c].Char.AsciiChar = rand() % 74 + 48;
-                            buffer[r][c].Attributes = rand() % 2 + 10;
+                        if(rand() % (250 - rate) == 0) {
+                            buffer[r][c].Char.AsciiChar = 31;
+                            buffer[r][c].Attributes = 2;
                         }
-                    } else {}
+                    } else {
+                        if(rand() % 5 == 0) {
+                            buffer[r][c].Char.AsciiChar = 32;
+                        }
+                    }
+                    //buffer[r][1].Char.AsciiChar = (rate/100) % 10 + 48;
+                    //buffer[r][2].Char.AsciiChar = (rate/10) % 10 + 48;
+                    //buffer[r][3].Char.AsciiChar = rate % 10 + 48;
                 } else {
                     buffer[r][c] = buffer[r - 1][c];
-                    if(!(buffer[r][c].Char.AsciiChar == 32))
-                        buffer[r][c].Char.AsciiChar = rand() % 74 + 48;
+                    //WriteConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize, dwBufferCoord, &rcRegion);
+                    if(!(buffer[r][c].Char.AsciiChar == 32)) {
+                        // ≡ = 240; ♥ = 3;
+                        if(buffer[r + 1][c].Char.AsciiChar == 32) {
+                            buffer[r][c].Attributes = 10;
+                        } else {
+                            buffer[r][c].Attributes = 2;
+                        }
+                    }
                 }
+                if(!(buffer[r][c].Char.AsciiChar == 32))
+                    buffer[r][c].Char.AsciiChar = rand() % 256;
             }
         }
+        if(rate < 240) rate++;
+        if(rate < 100) rate++;
         WriteConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize, dwBufferCoord, &rcRegion);
-        Sleep(75);
+        Sleep(250 - rate);
 
 
         /*for(r = 0; r < SCREEN_HEIGHT; r++)
