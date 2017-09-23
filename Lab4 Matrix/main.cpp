21f57a24baccbfa4cb756dad8c718be2f76c46f2
goofables <goofables@gmail.com>
2017-09-23 00:18:17 -0600

    Added RATE_LIMIT

--- Lab4 Matrix/main.cpp
+++ Lab4 Matrix/main.cpp
@@ -18,6 +18,7 @@
 #define SCREEN_HEIGHT 51
 #define COLOR2 2  //4
 #define COLOR1 10 //12
+#define RATE_LIMIT 240
 
 
 int main(void) {
@@ -43,14 +44,16 @@ int main(void) {
     Sleep(10000);*/
     ReadConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize, dwBufferCoord, &rcRegion);
     int rate = 175;
-    std::string text = "The matrix";
-    CHAR_INFO tempBuffer[text.length()];
+    std::string message = "The matrix";
+    CHAR_INFO tempBuffer[message.length()];
+    bool isChar[message.length()];
+    for(int i = 0; i < message.length(); i++) isChar[i] = false;
     while(true) {
         for(r = SCREEN_HEIGHT - 1; r >= 0; r--) {
             for(c = 0; c < SCREEN_WIDTH; c++) {
                 if(r == 0) {
                     if(buffer[r][c].Char.AsciiChar == 32) {
-                        if(rand() % (250 - rate) == 0)
+                        if(rand() % (RATE_LIMIT + 25 - rate) == 0)
                             buffer[r][c].Char.AsciiChar = 31;
                     } else {
                         if(rand() % 5 == 0)
@@ -63,23 +66,36 @@ int main(void) {
                     buffer[r][c] = buffer[r - 1][c];
                 if(!(buffer[r][c].Char.AsciiChar == 32)) {
                     buffer[r][c].Attributes = (buffer[r + 1][c].Char.AsciiChar == 32) ? COLOR1 : COLOR2;
-                    // ≡ = 240; ♥ = 3;
-                    buffer[r][c].Char.AsciiChar = rand() % 256;
+                    // ≡ = 240; ♥ = 3, ║ = 186;
+                    buffer[r][c].Char.AsciiChar =
+                        rand() % 256;
+                    //(buffer[r + 1][c].Char.AsciiChar == 32) ? 86 : 186;
+                    //240; // ≡
+                    //3;   // ♥
                 }
             }
         }
-        if(rate < 230) rate++;
+        if(rate < RATE_LIMIT) rate++;
         if(rate < 100) rate += 2;
-        if(rate == 230)
-            for(int i = 0; i < text.length(); i++) {
-                tempBuffer[i] = buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - (text.length() / 2) + i];
-                buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - (text.length() / 2) + i].Char.AsciiChar = text[i];
-                buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - (text.length() / 2) + i].Attributes = COLOR1;
+        if(rate == RATE_LIMIT)
+            for(int i = message.length() - 1; i >= 0; i--) {
+                /*if(!isChar[i]) {
+                    if(i == 0 | isChar[i - 1])
+                        if(buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - (message.length() / 2) + i].Char.AsciiChar ==
+                                message[i])
+                            isChar[i] = true;
+                } else {*/
+                tempBuffer[i] = buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - (message.length() / 2) + i];
+                buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - (message.length() / 2) + i].Char.AsciiChar =
+                    message[i];
+                buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - (message.length() / 2) + i].Attributes = COLOR1;
+                //}
             }
         WriteConsoleOutput(hOutput, (CHAR_INFO *)buffer, dwBufferSize, dwBufferCoord, &rcRegion);
-        if(rate == 230)
-            for(int i = 0; i < text.length(); i++)
-                buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - (text.length() / 2) + i] = tempBuffer[i];
+        if(rate == RATE_LIMIT)
+            for(int i = 0; i < message.length(); i++)
+                //if(isChar[i])
+                buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - (message.length() / 2) + i] = tempBuffer[i];
         Sleep(250 - rate);
     }
     return 0;
